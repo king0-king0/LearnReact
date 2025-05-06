9551dc96c669c972972919f5f73b490e74975b46
Your Name <you@example.com>
2025-05-06 01:10:01 -0900

    learnReact

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let ele = (
+  <div>
+    <h1 className="text-9xl absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">test</h1>
+  </div>
+);
+ReactDOM.render(ele, document.getElementById("app"));
