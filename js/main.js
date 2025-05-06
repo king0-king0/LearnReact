979fb789c4e7c235130f9a262b616cccaf92d92b
Your Name <you@example.com>
2025-05-06 01:16:14 -0900

    f

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+let num = 3;
 let ele = (
-  <div>
-    <h1 className="text-9xl absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">test</h1>
+  <div className=" absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
+    <div className=" flex flex-col *:m-3 items-center">
+      <h1 className="text-9xl">test {num}</h1>
+      <button className="bg-blue-500 w-1/3 rounded text-white">add</button>
+    </div>
   </div>
 );
 ReactDOM.render(ele, document.getElementById("app"));
